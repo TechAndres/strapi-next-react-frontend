14da58f9594fe8de4ffaffa6aadd83982790db78
Andres Valencia Nieto <carlos.valencia10@outlook.com>
2021-01-10 06:19:22 -0500

    Adds optional chaining to card rendering

--- components/card.js
+++ components/card.js
@@ -4,18 +4,18 @@ import Image from './image';
 
 const Card = ({ article }) => {
   return (
-    <Link as={`/article/${article.slug}`} href="/article/[id]">
+    <Link as={`/article/${article?.slug}`} href="/article/[id]">
       <a className="uk-link-reset">
         <div className="uk-card uk-card-muted">
           <div className="uk-card-media-top">
-            <Image image={article.image} />
+            <Image image={article?.image} />
           </div>
           <div className="uk-card-body">
             <p id="category" className="uk-text-uppercase">
-              {article.category.name}
+              {article.category?.name}
             </p>
             <p id="title" className="uk-text-large">
-              {article.title}
+              {article?.title}
             </p>
           </div>
         </div>
